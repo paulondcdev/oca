1f727e6b8b08b9a0d9cd9d2a2911c8016062146d
Paulo Nogueira <paulondcdev@gmail.com>
2017-02-27 04:47:52 -0800

    changed project structure

--- src/Bundle/Actions/FileOperation/index.js
+++
@@ -1,5 +0,0 @@
-module.exports.Move = require('./Move');
-module.exports.Delete = require('./Delete');
-module.exports.Copy = require('./Copy');
-module.exports.Checksum = require('./Checksum');
-module.exports.Download = require('./Download');
--- src/Bundle/index.js
+++
@@ -1,3 +0,0 @@
-module.exports.Inputs = require('./Inputs');
-module.exports.Actions = require('./Actions');
-module.exports.Providers = require('./Providers');
