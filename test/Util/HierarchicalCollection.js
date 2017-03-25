c9887d86a010d95198c60add27a4837cbbc26b04
Paulo Nogueira <paulon@Paulos-MacBook-Pro.local>
2017-03-25 14:43:11 -0700

    Added HirarchicalCollection which is used by the new support for metadata

---
+++ test/Util/HierarchicalCollection.js
@@ -0,0 +1,77 @@
+const assert = require('assert');
+const Oca = require('../../src');
+
+const HierarchicalCollection = Oca.Util.HierarchicalCollection;
+
+
+describe('Util HierarchicalCollection:', () => {
+
+  it('Should insert a value to the collection', () => {
+
+    const collection = new HierarchicalCollection();
+    const value = 10;
+
+    collection.insert('myKey', value);
+    assert.equal(collection.query('myKey'), value);
+  });
+
+  it('Should insert a multi level value to the collection', () => {
+
+    const collection = new HierarchicalCollection();
+    const value = 10;
+
+    collection.insert('a.b.c.d.myKey', value);
+    assert.equal(collection.query('a.b.c.d.myKey'), value);
+  });
+
+  it('Passing an empty string should return the entire collection', () => {
+
+    const collection = new HierarchicalCollection();
+
+    collection.insert('a', 10);
+    collection.insert('b', 12);
+
+    const result = collection.query('');
+
+    assert.equal(result.a, 10);
+    assert.equal(result.b, 12);
+  });
+
+  it('Should return the default value when value does not exist in collection', () => {
+
+    const collection = new HierarchicalCollection();
+    assert.equal(collection.query('does.not.exist', 20), 20);
+  });
+
+  it('Should merge value with existing (default option)', () => {
+
+    const collection = new HierarchicalCollection();
+
+    collection.insert('test.level', {
+      a: 1,
+    });
+    collection.insert('test.level', {
+      b: 2,
+    });
+
+    assert.equal(collection.query('test.level.a'), 1);
+    assert.equal(collection.query('test.level.b'), 2);
+  });
+
+  it('Should not merge value with existing', () => {
+
+    const collection = new HierarchicalCollection();
+
+    collection.insert('test.level', {
+      a: 1,
+    });
+    collection.insert('test.level', {
+      b: 2,
+    },
+      false,
+    );
+
+    assert.equal(collection.query('test.level.a'), undefined);
+    assert.equal(collection.query('test.level.b'), 2);
+  });
+});
